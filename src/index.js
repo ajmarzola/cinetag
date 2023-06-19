9b89d0e6e899b1222d4679924ee29c6a4b032085
Anderson Marzola <ajmarzola@gmail.com>
2023-06-19 16:19:14 -0300

    Correção do router-dom

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import AppRoutes from 'routes.js';
 import './index.css';
-import AppRoutes from 'routes';
 
-ReactDOM.render(
-  <AppRoutes />
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <AppRoutes />
+  </React.StrictMode>
 );
