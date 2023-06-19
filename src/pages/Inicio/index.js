450138f9786c493975dc60ce55c00067e398dbf9
Anderson Marzola <ajmarzola@gmail.com>
2023-06-19 18:01:24 -0300

    Favoritos e reaproveitamento do Banner e Rodapé

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,20 +1,23 @@
 import Banner from "components/Banner";
-import Cabecalho from "components/Cabecalho";
 import Card from "components/Card";
-import Rodape from "components/Rodape";
 import Titulo from "components/Titulo";
+import videos from 'json/db.json';
+import styles from './Inicio.module.css';
 
-function Inicio (){
+function Inicio() {
     return (
         <>
-            <Cabecalho />
             <Banner imagem="home" />
-            <Titulo><h1>Um lugar para guardar seus vídeos e filmes!</h1></Titulo>
-            <Card id="1" titulo="Vingadores" capa="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/q6725aR8Zs4IwGMXzZT8aC8lh41.jpg" />
-            <br/>
-            <Rodape />
+            <Titulo>
+                <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+            </Titulo>
+            <section className={styles.container}>
+                {videos.map((video) => {
+                    return <Card {...video} key={video.id} />
+                })}
+            </section>
         </>
-    );
+    )
 }
 
 export default Inicio;
